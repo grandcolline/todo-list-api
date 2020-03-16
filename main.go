b8f98c76092d7e88388f38a20ac637e2fe35842a
Taiki Hayashi <grandcolline@gmail.com>
2020-03-17 00:25:13 +0900

    整理

--- main.go
+++ main.go
@@ -1,51 +1,17 @@
 package main
 
 import (
-	// "fmt"
-	// "context"
-	// "log"
-
-	// firebase "firebase.google.com/go"
-	// "google.golang.org/api/option"
 	"github.com/grandcolline/todo-list-api/driver"
 )
 
 func main() {
 	// ロガーファクトリの作成
 	driver.InitLog()
-
 	// Gatewayの作成
 	driver.InitGateway()
-
 	// タスクコントローラの作成
 	driver.InitController()
 
+	// サーバの起動
 	driver.Serve()
 }
-
-//func main() {
-//	// Use a service account
-//	ctx := context.Background()
-//	sa := option.WithCredentialsFile("./grandcolline-dev-87202938d0c8.json")
-//	app, err := firebase.NewApp(ctx, nil, sa)
-//	if err != nil {
-//		log.Fatalln(err)
-//	}
-//
-//	client, err := app.Firestore(ctx)
-//	if err != nil {
-//		log.Fatalln(err)
-//	}
-//
-//	// データ追加
-//	_, _, err = client.Collection("task").Add(ctx, map[string]interface{}{
-//		"name":   "Ada",
-//		"status": "Lovelace",
-//	})
-//	if err != nil {
-//		log.Fatalf("Failed adding alovelace: %v", err)
-//	}
-//
-//	// 切断
-//	defer client.Close()
-//}
